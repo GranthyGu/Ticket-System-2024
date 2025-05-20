ae2881fe4eb24b3b463b0dce9a1e2a9071f930e4
GranthyGu <granthygu@sjtu.edu.cn>
2025-05-21 03:01:55 +0800

    Update for correcting the bug in refund function

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 #include "train_management.hpp"
 
 int main() {
-    freopen("in.txt", "r", stdin);   // 读入in.dat文件作为输入 
-	freopen("out.dat", "w", stdout);  // 将输入写入out.dat文件中 
+    // freopen("in.txt", "r", stdin);   // 读入in.dat文件作为输入 
+	// freopen("out.dat", "w", stdout);  // 将输入写入out.dat文件中 
     train_management train;
     account_management account;
     ticket_management ticket;
