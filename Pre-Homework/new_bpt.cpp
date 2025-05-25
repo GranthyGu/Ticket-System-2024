346695fb977d723638a8045f34c4cb1b5a50e72f
GranthyGu <granthygu@sjtu.edu.cn>
2025-05-25 16:46:37 +0800

    Update for bpt size.

--- Pre-Homework/new_bpt.cpp
+++ Pre-Homework/new_bpt.cpp
@@ -634,7 +634,7 @@ public:
 int main() {
     int n;
     std::cin >> n;
-    B_plus_tree<key_value, char, 3, 3> bpt("File_for_bpt");
+    B_plus_tree<key_value, char, 45, 45> bpt("File_for_bpt");
     for (int i = 0; i < n; i++) {
         std::string operation;
         std::cin >> operation;
